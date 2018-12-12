f8285ba666ce7ca3fad8fdc334952c2e67bdfca7
HamCell <Ilham.webseo@gmail.com>
2018-12-12 13:29:06 +0700

    Add files via upload

---
+++ shellv.php
@@ -0,0 +1,261 @@
+<!doctype html>
+<html  >
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Login</title>
+
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<link REL="stylesheet" href="styles/default.css" type="text/css">
+		
+		
+		<link REL="stylesheet" href="include/bootstrap/css/bootstrap.min.css" type="text/css">
+		
+		<link REL="stylesheet" href="styles/bootstrap/cosmo/css/bootstrap-theme.min.css" type="text/css">
+		
+		<link REL="stylesheet" href="styles/bs.css" type="text/css">
+		
+		<link REL="stylesheet" href="styles/bootstrap/cosmo/css/tweaks.css" type="text/css">
+		
+		<link REL="stylesheet" href="include/jquery-ui/smoothness/jquery-ui.min.css" type="text/css">
+		
+		<link REL="stylesheet" href="include/bootstrap/css/jquery.mCustomScrollbar.css" type="text/css">
+		
+	</head>
+
+	<body class="OfficeDeliciousLavender page-login_bootstrap function-login" >
+	<script type="text/javascript" src="include/loadfirst.js"></script><script type="text/javascript" src="include/lang/Indonesian.js"></script><form method="post" action='login.php' id="form1" name="form1">
+								<input type="hidden" name="btnSubmit" value="Login">
+		 
+<P align="center"><BR></P>
+<P align="center"><IMG width="163" height="31" style="width: 319px; height: 57px;" src="images/alumni1.png" border="0"></P>
+		<style>
+			
+		</style>
+		<DIV class="bs-top">
+        
+
+
+<div  data-pageid="1" data-container="page" class="container    "> 
+
+
+
+
+
+<div  data-pageid="1" data-container="page_1" class=" bs-standalone-page  col-md-6 col-md-offset-3"> 
+
+
+
+
+
+<div  data-pageid="1" data-container="panel" class=" bs-pagepanel panel panel-primary   "> 
+
+
+
+
+
+<div  data-pageid="1" data-container="header" class=" panel-heading   "> 
+
+
+<h3 class="panel-title"  data-pageid="1" data-brick="loginheader" >
+
+	Login
+</h3>
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+<div  data-pageid="1" data-container="body" class=" panel-body   "> 
+
+
+
+
+
+<div  data-pageid="1" data-container="header_1" class="   col-md-12 "> 
+
+
+<div  data-pageid="1" data-brick="message" class="alert alert-danger" role="alert">Your session has expired.Please login again.</div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+<div  data-pageid="1" data-container="fields" class="   col-md-12 "> 
+
+
+<div  data-pageid="1" data-brick="loginfields" class="form-horizontal" role="form">
+	<div class="form-group" >
+		<label class="col-md-4 control-label" for="username">
+			Username:
+		</label>
+		<div class="col-md-6" >
+			<input type="text" class="form-control" name="username" value="" id="username">
+		</div>
+	</div>
+	<div class="form-group" >
+		<label class="col-md-4 control-label" for="password">
+			Password:
+		</label>
+		<div class="col-md-6" >
+			<input type="password" class="form-control" name="password"  value="" id="password">
+		</div>
+	</div>
+	<div class="form-group" >
+		<div class="col-md-6 col-md-offset-4" >
+			<div class="checkbox">
+				<label>
+					<input type="checkbox" name="remember_password" value="1"> <span>Remember me</span>
+				</label>
+			</div>
+		</div>
+	</div>
+	
+</div>
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+<div  data-pageid="1" data-container="header_2" class="   col-md-8 col-md-offset-4"> 
+
+
+<button class="btn btn-primary" id="submitLogin1">Sign in</button>
+
+
+
+<div class="btn btn-link"><a href="menu.php" >Login sebagai Tamu</a></div>
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+<div  data-pageid="1" data-container="register" class="  text-center col-md-12 "> 
+
+
+<span  data-pageid="1" data-brick="bsloginregister">
+	<a href="register.php" class="btn btn-link btn-xs" >Sign Up</a>
+</span>
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+</div>
+
+
+
+		</div>
+		<div class="bs-middle ">
+			<div class="bs-topmiddle">
+				
+			</div>
+			<div class="bs-left" >
+				
+			</div>
+			<div class="bs-center">
+				
+			</div>
+			<div class="bs-right">
+				
+			</div>
+		</div>
+		<DIV class="bs-bottom">
+        
+		</div>
+		<STYLE>
+body {
+        background: url(Alumni.jpg)no-repeat center center;
+ -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+}
+</STYLE>
+
+
+	</form><script>window.controlsMap = {".global":{"login":{"1":{"oldLayout":false,"layoutVersion":3,"layoutName":"OfficeDeliciousLavender","video":[],"toolTips":[],"searchPanelActivated":false,"controls":[],"search":{"searchBlocks":[],"allSearchFields":[],"allSearchFieldsLabels":[],"panelSearchFields":[],"googleLikeFields":[],"inflexSearchPanel":true,"requiredSearchFields":[],"isSearchRequired":false,"searchTableName":".global","shortSearchTableName":"","submitPageType":"login"},"gMaps":{"isUseMainMaps":false,"isUseFieldsMaps":false,"isUseGoogleMap":false,"APIcode":"","mainMapIds":[],"fieldMapsIds":[],"mapsData":[]}}}}};window.viewControlsMap = {".global":{"login":{"1":[]}}};window.settings = {"tableSettings":{".global":{"proxy":{"proxy":""},"fieldSettings":{"Id":{"login":{"strName":"Id","editFormat":"Text field","mask":false,"maxFileSize":false,"maxTotalFilesSize":false}},"id_akun":{"login":{"strName":"id_akun","editFormat":"Text field","mask":false,"maxFileSize":false,"maxTotalFilesSize":false}},"nis":{"login":{"strName":"nis","editFormat":"Text field","mask":false,"maxFileSize":false,"maxTotalFilesSize":false}},"Nama_Lengkap":{"login":{"strName":"Nama_Lengkap","editFormat":"Text field","mask":false,"maxFileSize":false,"maxTotalFilesSize":false}},"jurusan":{"login":{"strName":"jurusan","editFormat":"Lookup wizard","mask":false,"lookupTable":"jurusan","linkField":"jurusan","dispField":"jurusan","maxFileSize":false,"maxTotalFilesSize":false}},"tahun_lulus":{"login":{"strName":"tahun_lulus","editFormat":"Lookup wizard","mask":false,"lookupTable":"tahun","linkField":"tahun","dispField":"tahun","maxFileSize":false,"maxTotalFilesSize":false}},"kerja_di":{"login":{"strName":"kerja_di","editFormat":"Text field","mask":false,"maxFileSize":false,"maxTotalFilesSize":false}},"Kulia_di":{"login":{"strName":"Kulia_di","editFormat":"Text field","mask":false,"maxFileSize":false,"maxTotalFilesSize":false}},"foto":{"login":{"strName":"foto","editFormat":"Document upload","viewFormat":"Document Download","mask":false,"maxFileSize":false,"maxTotalFilesSize":false}},"alamat":{"login":{"strName":"alamat","editFormat":"Text area","mask":false,"maxFileSize":false,"maxTotalFilesSize":false}},"no_telepon":{"login":{"strName":"no_telepon","editFormat":"Text field","mask":false,"maxFileSize":false,"maxTotalFilesSize":false}},"alamat_website":{"login":{"strName":"alamat_website","editFormat":"Text field","mask":false,"maxFileSize":false,"maxTotalFilesSize":false}},"alamat_fb_twiter":{"login":{"strName":"alamat_fb_twiter","editFormat":"Text field","mask":false,"maxFileSize":false,"maxTotalFilesSize":false}},"user":{"login":{"strName":"user","editFormat":"Text field","mask":false,"maxFileSize":false,"maxTotalFilesSize":false}},"password":{"login":{"strName":"password","editFormat":"Password","mask":false,"maxFileSize":false,"maxTotalFilesSize":false}}},"strCaption":null,"pageMode":0,"listGridLayout":3,"isUseHighlite":true,"isUseToolTips":true,"isUseCK":false}},"global":{"shortTNames":{".global":"","jurusan":"jurusan","tahun":"tahun"},"webRootPath":"","ext":"php","charSet":"utf-8","curretLang":"Indonesian","debugMode":false,"googleMapsApiCode":"","popupPagesLayoutNames":{"login":"login_bootstrap","register":"register_bootstrap"},"isMobile":false,"mobileDeteced":false,"s508":false,"mapProvider":0,"locale":{"dateFormat":"1","langName":"id","ctryName":"ID","startWeekDay":"0","dateDelimiter":"\/","is24hoursFormat":"1","leadingZero":"0","timeDelimiter":":","timePmLetter":"","timeAmLetter":""},"showDetailedError":true,"customErrorMessage":"","loginFormType":0,"idStartFrom":2}};</script><script type="text/javascript" src="include/runnerJS/RunnerAll.js"></script><script>Runner.util.ScriptLoader.addJS(['include/bootstrap/js/bootstrap.min.js']);
+Runner.util.ScriptLoader.addJS(['include/jquery-ui/jquery-ui.min.js']);
+Runner.util.ScriptLoader.addJS(['include/bootstrap/js/jquery.mCustomScrollbar.min.js']);
+Runner.util.ScriptLoader.addJS(['include/ResizeSensor/ResizeSensor.js']);
+ Runner.util.ScriptLoader.load();</script>
+	</body>
+</html>
